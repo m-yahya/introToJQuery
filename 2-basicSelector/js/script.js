1718558a81861681648228c367fb5ff876f26086
Muhammad Yahya <yahya_gis@hotmail.com>
2018-01-11 18:27:16 +0100

    contents added

--- 2-basicSelector/js/script.js
+++ 2-basicSelector/js/script.js
@@ -1,14 +1,14 @@
 $(document).ready(function() {
-// jQuery selectors
-$('*'); // select all
-$('div').css('background-color', 'lightgray'); // select by element tag
-$('h1').css('color', 'orange'); // select by element tag
-$('#idExample').css('color', 'blue'); // select by id
-$('.classExample').css('color', 'green'); // select by class
-/*$('tag.classExample') // select element that has classExample
-$('tag.#idExample'); // select element that has idExample*/
-// selecting all elements
-var element = $('*').length;
-// append the length to the body as heading 3
-$( "body" ).append( "<h3> There are " + element + " elements selected.</h3>" );
+  // jQuery selectors
+  $('*'); // select all
+  $('div').css('background-color', 'lightgray'); // select by element tag
+  $('h1').css('color', 'orange'); // select by element tag
+  $('#idExample').css('color', 'blue'); // select by id
+  $('.classExample').css('color', 'green'); // select by class
+  /*$('tag.classExample') // select element that has classExample
+  $('tag.#idExample'); // select element that has idExample*/
+  // selecting all elements
+  var element = $('*').length;
+  // append the length to the body as heading 3
+  $( "body" ).append( "<h3> There are " + element + " elements selected.</h3>" );
 });
--- README.md
+++ README.md
@@ -1,1 +1,50 @@
-# introToJQuery
+## Introduction to JavaScript
+
+Introduction to jQuery is an introductory level presentation for the beginners that is aimed at *Write Less, Do More*.
+
+[Overview](https://m-yahya.github.io/introToJQuery/)
+
+
+***
+
+## Code Example
+
+The source codes are simple examples to understand the concept of basics in jQuery.
+
+```javascript
+$(document).ready(function() {
+  // code will go here
+  $('h1').slideUp(1000, 'swing').fadeIn(1000, 'swing');
+});
+```
+
+***
+
+## Motivation
+
+The project is aimed to explain the basics in a simple and easy way to make them understandable for the beginners.
+
+***
+
+## Feature and Limitations
+
+- The projects includes basics of jQuery
+- Basic Selecting Techniques in jQuery
+- Basic Filtering in jQuery
+- DOM Manipulation
+- Effects and Animation in jQuery
+- Concepts are explained at beginners level
+
+***
+
+## Tests
+
+The example codes can either be run by internally or externally embedding jQuery files to HTML file. On flip side, browser console can be used to run the code on fly and see the results in log.
+
+***
+
+## Contributors
+
+New ideas, suggestions and improvement are welcome.
+
+***
